348132423107493046dd7f9cff6d0b4995c0b083
Graham Borland <graham.borland@gramboid.com>
2016-04-20 15:34:54 +0100

    Add sample code for subscribing in an Activity

--- rxappfocus-sample/src/main/java/com/example/rxappfocus/App.java
+++ rxappfocus-sample/src/main/java/com/example/rxappfocus/App.java
@@ -24,4 +24,8 @@ public class App extends Application {
                     }
                 });
     }
+
+    public AppFocusProvider getFocusProvider() {
+        return focusProvider;
+    }
 }
--- rxappfocus-sample/src/main/java/com/example/rxappfocus/MainActivity.java
+++ rxappfocus-sample/src/main/java/com/example/rxappfocus/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.rxappfocus;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.gramboid.rxappfocus.AppFocusProvider;
+
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Subscription     sub;
+    private AppFocusProvider focusProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        focusProvider = ((App) getApplication()).getFocusProvider();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sub = focusProvider
+                .getAppFocus()
+                .filter(new Func1<Boolean, Boolean>() {
+                    @Override public Boolean call(Boolean visible) {
+                        return visible;
+                    }
+                })
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean visible) {
+                        Log.d("MainActivity", "We are visible!");
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sub.unsubscribe();
     }
 }
