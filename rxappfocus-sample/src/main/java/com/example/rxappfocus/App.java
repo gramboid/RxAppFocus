c60a2cbc4aefdc09f2664406d2797a7deae0fa36
Graham Borland <graham.borland@gramboid.com>
2017-11-24 17:26:55 +0000

    Update sample to Rx2

--- rxappfocus-sample/src/main/java/com/example/rxappfocus/App.java
+++ rxappfocus-sample/src/main/java/com/example/rxappfocus/App.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 import com.gramboid.rxappfocus.AppFocusProvider;
 
-import rx.functions.Action1;
+import io.reactivex.functions.Consumer;
 
 public class App extends Application {
 
@@ -17,10 +17,10 @@ public class App extends Application {
         focusProvider = new AppFocusProvider(this);
 
         // show a toast every time the app becomes visible or hidden
-        focusProvider.getAppFocus()
-                .subscribe(new Action1<Boolean>() {
+        focusProvider.getAppFocus2()
+                .subscribe(new Consumer<Boolean>() {
                     @Override
-                    public void call(Boolean visible) {
+                    public void accept(Boolean visible) {
                         Toast.makeText(App.this, visible ? "App visible" : "App hidden", Toast.LENGTH_SHORT).show();
                     }
                 });
