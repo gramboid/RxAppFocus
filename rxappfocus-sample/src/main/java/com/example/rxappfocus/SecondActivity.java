f1591937c8996bdfa3f86383bc5ceb19431466b8
Graham Borland <graham.borland@gramboid.com>
2019-02-08 21:00:52 +0000

    Migrate to AndroidX

--- rxappfocus-sample/src/main/java/com/example/rxappfocus/SecondActivity.java
+++ rxappfocus-sample/src/main/java/com/example/rxappfocus/SecondActivity.java
@@ -1,6 +1,6 @@
 package com.example.rxappfocus;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
--- rxappfocus/src/main/java/com/gramboid/rxappfocus/AppFocusProvider.java
+++ rxappfocus/src/main/java/com/gramboid/rxappfocus/AppFocusProvider.java
@@ -3,8 +3,8 @@ package com.gramboid.rxappfocus;
 import android.app.Activity;
 import android.app.Application;
 import android.app.Application.ActivityLifecycleCallbacks;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 
 /**
