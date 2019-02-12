d3ca08907470286eb4ad7d6feada59e029d3cbc3
Graham Borland <graham.borland@gramboid.com>
2019-02-12 11:54:58 +0000

    Add comment

--- rxappfocus/src/main/java/com/gramboid/rxappfocus/AppFocusProvider.java
+++ rxappfocus/src/main/java/com/gramboid/rxappfocus/AppFocusProvider.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
  */
 public class AppFocusProvider {
 
+    // This doesn't actually leak the visibleActivity since we null it out
     @SuppressLint("StaticFieldLeak")
     private static final AppFocusProvider instance = new AppFocusProvider();
 
